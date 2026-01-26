1a34c503f6134dc9d6d084f5b32370dbaf23871d
PinGoo-AppWeb <app.pingoo@gmail.com>
2026-01-26 19:08:03 -0300

    melhoriais de seguança .env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jbkhnucisqfjkbwnqfab.supabase.co';
-const supabaseAnonKey = 'sb_publishable_K5YDpxSxeJtwhTKPPrikdA_dKLLu2zF';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
