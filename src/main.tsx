7a0d74e49a24cb60aa04e5398f02971c593d2530
PinGoo-AppWeb <app.pingoo@gmail.com>
2026-01-26 20:43:02 -0300

    mudanças de tipogafia reforçadas

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import "./index.css";
 import { registerSW } from "virtual:pwa-register";
 
 // Register PWA service worker
-registerSW({ immediate: true });
+registerSW({
+    immediate: true,
+    onNeedRefresh() {
+        if (confirm("Nova versão disponível! Atualizar agora?")) {
+            window.location.reload();
+        }
+    },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
